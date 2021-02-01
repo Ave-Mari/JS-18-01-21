cfe52cd04044d69ce144654e55f64800f8fe7bca
Mari Vaypan <avemari98@gmail.com>
2021-02-01 10:21:05 +0300

    finished

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -16,7 +16,21 @@
    isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
-function isAllTrue(array, fn) {}
+function isAllTrue(array, fn) {
+  if (typeof fn !== 'function') {
+    throw new Error("fn is not a function");
+  };
+  if (!Array.isArray(array) || array.length) {
+    throw new Error("empty array"); 
+  };
+
+  for (var element of array) {
+    if (!fn(element)) {
+      return false;
+    }
+  };
+  return true; 
+};
 
 /*
  Задание 2:
@@ -34,7 +48,21 @@ function isAllTrue(array, fn) {}
    isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
-function isSomeTrue(array, fn) {}
+function isSomeTrue(array, fn) {
+  if (!Array.isArray(array) || array.length) {
+    throw new Error("empty array"); 
+  };
+  if (typeof fn !== 'function') {
+    throw new Error("fn is not a function");
+  };
+  for (var element of array) {
+    if (fn(element)) {
+      return  true;
+    }
+  }
+
+  return false;
+};
 
 /*
  Задание 3:
@@ -47,7 +75,21 @@ function isSomeTrue(array, fn) {}
  3.3: Необходимо выбрасывать исключение в случаях:
    - fn не является функцией (с текстом "fn is not a function")
  */
-function returnBadArguments(fn, ...args) {}
+function returnBadArguments(fn, ...args) {
+  if (typeof fn !== 'function') {
+    throw new Error("fn is not a function");
+  };
+  var array = []; 
+  for (var arg in args) {
+    try {
+      fn(arg);
+    } catch {
+      array.push(arg);
+    }
+  }
+
+  return array;  
+};
 
 /*
  Задание 4:
@@ -66,7 +108,38 @@ function returnBadArguments(fn, ...args) {}
    - number не является числом (с текстом "number is not a number")
    - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
-function calculator(number = 0) {}
+function calculator(number = 0) {
+  if (typeof isNaN(number)) { 
+    throw new Error("number is not a number");
+  };
+  return {
+    sum(...arguments) {
+      var result = number;
+      for (var argument of arguments) {
+        result += number; 
+      }
+    },
+    dif(...arguments) {
+      var result = number; 
+      for (var argument of arguments) {
+        result -= argument;
+      }
+    },
+    div(...arguments) {      
+      var result = number;
+      for (var argument of arguments) {
+        if (argument === 0) {
+          throw new Error("division by 0"); 
+        }
+        result /= argument;
+      }
+    },
+    mul(...arguments) { 
+      var result = number; 
+      result *= argument;
+    }
+  }  
+};
 
 /* При решении задач, постарайтесь использовать отладчик */
 
