d987b72959f34e75a44328a0a19efae69154514d
Mari Vaypan <avemari98@gmail.com>
2021-02-15 10:53:21 +0300

    finished

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,8 @@ module.exports = {
         options: { cacheDirectory: true },
       },
       {
-        test: /projects\/.+\.html/,
+        test: /\.html$/,
+        include: [path.resolve(__dirname, 'projects')],
         use: [
           { loader: './scripts/html-inject-loader.js' },
           {
